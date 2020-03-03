3aea49e7ad209ecfe044e390d99e6bfc12ada10a
Roi <vosroix@gmail.com>
2020-03-03 16:32:14 +0500

    added new plugin

--- wp-content/themes/sparrow/404.php
+++ wp-content/themes/sparrow/404.php
@@ -1,1 +1,23 @@
-Ошибка! Такой страницы не существует
+<?php get_header() ?>
+<!-- Page Title
+================================================== -->
+<div id="page-title">
+
+    <div class="row">
+
+        <div class="ten columns centered text-center">
+        <h1><span>Page not found</span></h1>
+
+        <p>Error 404</p>
+        </div>
+
+    </div>
+
+</div> <!-- Page Title End-->
+
+
+</section> <!-- Tweets Section End-->
+
+<!-- footer
+================================================== -->
+<?php get_footer()?>
